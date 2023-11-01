2062605235e9a19b615dc61b35481bf99bfc8a1f
Alex <89515014507solra@gmail.com>
2023-11-02 04:55:55 +0600

    docker build setup

--- src/main/resources/db/migration/V1.1698779627__create_table_user_db.sql
+++ src/main/resources/db/migration/V1.1698779627__create_table_user_db.sql
@@ -1,6 +1,6 @@
 create table if not exists users(
-                                    id bigserial primary key ,
-                                    login varchar(20) unique not null ,
+    id bigserial primary key ,
+    login varchar(20) unique not null ,
     password varchar(50) not null ,
     email varchar unique not null ,
     date_create timestamp with time zone default now()
