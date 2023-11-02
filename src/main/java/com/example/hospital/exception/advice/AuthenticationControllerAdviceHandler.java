d8325ee27e21c1fe2c5b22be7474fa1d69245d03
Alex <89515014507solra@gmail.com>
2023-11-03 04:19:39 +0600

    removed excess garbage

--- src/main/java/com/example/hospital/exception/advice/AuthenticationControllerAdviceHandler.java
+++ src/main/java/com/example/hospital/exception/advice/AuthenticationControllerAdviceHandler.java
@@ -4,11 +4,9 @@ import com.example.hospital.controllers.v1.AuthenticationController;
 import com.example.hospital.exception.ExceptionModel;
 import com.example.hospital.exception.ResponseModel;
 import com.example.hospital.exception.exceptions.*;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = {AuthenticationController.class})
